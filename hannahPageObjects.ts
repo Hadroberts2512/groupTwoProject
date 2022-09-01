57a201d9090ac1c1ad2bdd93460c59d701801427
Hadroberts2512 <hannahaprilroberts@gmail.com>
2022-08-31 20:05:53 -0700

    this is my final product. One is a bug and SHOULD fail

--- hannahPageObjects.ts
+++ hannahPageObjects.ts
@@ -12,8 +12,17 @@ export class HannahPage extends BasePage {
     item: By = By.className('product-item-link')
     addtoCart: By = By.id('product-addtocart-button')
     cartIcon: By = By.className('action showcart active')
+    shortsSize: By = By.id('option-label-size-143-item-171')
+    shortsColorOrange: By = By.id('option-label-color-93-item-56')
+    numberInCart: By = By.xpath('//span[@class="counter-number"]')
+    proceedToCheckout: By = By.id('top-cart-btn-checkout')
+    wholePage: By = By.className('page-wrapper')
+    searchResultMessage: By = By.className('message notice')
+    mensShirtColor: By = By.id('option-label-color-93-item-49')
+
+
 
     constructor() {
-        super({url: 'https://magento.softwaretestingboard.com/customer/account/login/referer/aHR0cHM6Ly9tYWdlbnRvLnNvZnR3YXJldGVzdGluZ2JvYXJkLmNvbS8%2C/'})
+        ;super({url: 'https://magento.softwaretestingboard.com/customer/account/login/referer/aHR0cHM6Ly9tYWdlbnRvLnNvZnR3YXJldGVzdGluZ2JvYXJkLmNvbS8%2C/'})
     }
 }
