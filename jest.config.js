e86d399a8ab692e2b20ed28ee053b587b6679006
KoffiD <assiehuie@gmail.com>
2022-09-01 20:31:50 -0600

    codeTest2

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,5 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-<<<<<<< HEAD
   testEnvironment: 'node',
-
-=======
-  testEnvironment: 'node', 
-  testTimeout: 100000
->>>>>>> 57a201d9090ac1c1ad2bdd93460c59d701801427
 };
--- koffiTest.test.ts
+++ koffiTest.test.ts
@@ -1,29 +1,21 @@
-
 import { koffiPage } from "./koffiPageObject";
 const koffi = new koffiPage
 
 test('searching item and adding review', async() => {
-    /*await koffi.navigate('https://magento.softwaretestingboard.com/radiant-tee.html')
-    await koffi.radiantTee
-    await koffi.click(koffi.reviewbtn)
-    await koffi.click(koffi.ratingStar)
-    await koffi.setInput(koffi.nickname,"koffi")
-    await koffi.navigate()
-    await koffi.setInput(koffi.searchBtn,'Radiant Tee\n')
-    await koffi.hotSellers
-    await koffi.click(koffi.radiantTee)*/
-    
     await koffi.navigate()
     await koffi.setInput(koffi.searchInput,'Radiant Tee\n')
     await koffi.click(koffi.radiantTee)
     await koffi.click(koffi.radiantTee)
     await koffi.click(koffi.reviewTitle)
     //trying to give 4 stars but this code is not working
-    await koffi.click(koffi.rating)
+
     await koffi.setInput(koffi.nicknameInput,'koffi')
     await koffi.setInput(koffi.summaryInput,'Color of outfit')
     await koffi.setInput(koffi.reviewInput,'I really loved the way this was made. That is the texture and material. Different colors makes it look really cool.')
     await koffi.click(koffi.submitBtn)
+    let errroMessage = await koffi.getText(koffi.ratingError)
+    expect(errroMessage).toContain('Please select one of each of the ratings above.')
+   
     
    
 
@@ -33,4 +25,6 @@ test('searching item and adding review', async() => {
     
     
 })
+
+
 
